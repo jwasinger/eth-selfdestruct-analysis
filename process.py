ae02586fe79377bbab418e63e990a8caf86bab20
Jared Wasinger <j-wasinger@hotmail.com>
2022-03-02 19:49:16 -1000

    more

--- process.py
+++ process.py
@@ -2,23 +2,51 @@ reincarnations = {}
 ephemerals = {}
 deleted = {}
 
+def parse_callstack(callstack_field: str) -> [str]:
+    pass
+
+def parse_tx_index(tx_index_field: str) -> int:
+    splitted = tx_index_field.split('_')[2:]
+    splitted = map(lambda x: int(x), splitted)
+    return splitted
+
+def sort_tx_calls(calls):
+    max_call_depth = 1
+    for call in calls:
+        if len(calls.tx_id) > max_call_depth:
+            max_call_depth = len(calls.tx_id)
+
+    def score_fn(call) -> int:
+        score = 0
+        for i in range(len(call.call_id)):
+            score += call.call_id * 10**(max_call_depth - i)
+        return score
+
+    def sort_fn(x, y) -> int:
+        return score_fn(x.call_id) < score_fn(y.call_id)
+
+    return sorted(lambda x,y: sort_fn(x,y), calls)
+
 class MessageCall():
-    def __init__(self, status, typ, sender, receiver, callstack):
+    def __init__(self, status, typ, sender, receiver, tx_index, call_depth):
         self.status = status
         self.type = typ 
         self.sender = sender
         self.receiver = receiver
-        self.callstack = callstack 
-        self.tx_index = tx_index
+        self.tx_id = tx_id
+        self.call_depth = call_depth
 
     def FromCSVLine(s: str) -> Self:
-        status = ''
-        sender = ''
-        receiver = ''
-        callstack = []
-        tx_index = 0
+        parts = s.split(',')
 
-        # TODO parse
+        if len(parts) != 5:
+            raise Exception("wrong length")
+
+        status = parts[0]
+        sender = parts[1]
+        receiver = parts[2]
+        
+        tx_index = parse_tx_index(parts[4])
 
         return MessageCall(status, sender, receiver, callstack, tx_index)
 
@@ -33,12 +61,13 @@ def consume_transaction(lines):
         else:
             calls.append(line)
 
+    if len(txs) > 1:
+        # order txs
+
     # revert of the top-level call, move to the next tx
     if calls[0]['status'] == '1':
         return len(txs)
 
-    if len(txs) > 1:
-        # order txs
 
     for call in calls:
         # if the call failed, continue
