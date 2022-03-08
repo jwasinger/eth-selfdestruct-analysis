2e457e8cc5699f2a03327a0d5b3c3817cac0b8ca
Jared Wasinger <j-wasinger@hotmail.com>
2022-03-08 12:56:58 +0000

    wip

--- process.py
+++ process.py
@@ -1,3 +1,4 @@
+import itertools
 import functools
 import glob
 
@@ -8,22 +9,18 @@ created = set()
 total_lines = 0
 
 def sort_tx_calls(calls):
-    max_call_depth = 1
-    for call in calls:
-        if len(call.trace_id) > max_call_depth:
-            max_call_depth = len(call.trace_id)
-
-    def score_fn(trace_id) -> int:
-        score = 0
-        for i in range(len(trace_id)):
-            score += trace_id[i] * 10**(max_call_depth - i)
-        return score
-
     def sort_fn(x, y) -> int:
-        if score_fn(x.trace_id) < score_fn(y.trace_id):
-            return -1
-        else:
-            return 1
+        for t1, t2 in itertools.zip_longest(x.trace_id, y.trace_id):
+            if t2 == None:
+                return 1
+            elif t1 == None:
+                return -1
+
+            if t1 > t2:
+                return 1
+            elif t1 < t2:
+                return -1
+        return 0
 
     return sorted(calls, key=functools.cmp_to_key(sort_fn))
 
@@ -37,7 +34,8 @@ def parse_trace_id(s: str):
     return parts
 
 class MessageCall():
-    def __init__(self, tx_hash, tx_index, trace_id, sender, receiver, typ, status):
+    def __init__(self, block_number, tx_hash, tx_index, trace_id, sender, receiver, typ, status, call_type):
+        self.block_number = block_number
         self.tx_hash = tx_hash
         self.tx_index = tx_index
         self.trace_id = trace_id
@@ -45,26 +43,29 @@ class MessageCall():
         self.receiver = receiver
         self.type = typ
         self.status = status
+        self.call_type=call_type
 
     @staticmethod
     def FromCSVLine(s: str):
         parts = s.split(',')
 
-        if len(parts) != 10:
+        if len(parts) != 11:
             raise Exception("wrong length")
 
+        block_number = int(parts[0])
         tx_hash = parts[2]
         tx_index = int(parts[3])
         trace_id = parse_trace_id(parts[4])
         sender = parts[5]
         receiver = parts[6]
         typ = parts[7]
-        status = int(parts[8])
+        status = int(parts[9])
+        calltype = parts[8]
 
-        return MessageCall(tx_hash, tx_index, trace_id, sender, receiver, typ, status)
+        return MessageCall(block_number, tx_hash, tx_index, trace_id, sender, receiver, typ, status, calltype)
 
     def ToCSVLine(self):
-        return "{},{},{},{},{},{}".format(self.tx_hash, self.tx_index, self.sender, self.sender, self.type, self.status)
+        return "{},{},{},{},{},{},{}".format(self.tx_hash, self.tx_index, self.sender, self.sender, self.trace_id, self.call_type, self.type, self.status)
 
 tx_calls = []
 tx_created = set()
@@ -77,7 +78,7 @@ def process_transaction():
 
     # sort calls
     if len(tx_calls) > 1:
-        sort_tx_calls(tx_calls)
+        tx_calls = sort_tx_calls(tx_calls)
 
     for call in tx_calls:
         if call.status == 0:
@@ -85,6 +86,9 @@ def process_transaction():
 
         if call.type == 'create':
             # TODO what happens to self-destruct inside of create ?
+            if call.receiver == '':
+                import pdb; pdb.set_trace()
+                baz = 'bat'
             tx_created.add(call.receiver)
         else: # call is selfdestruct
             if not call.sender in tx_selfdestructed and not call.sender in tx_ephemerals:
@@ -95,6 +99,7 @@ def process_transaction():
                     tx_selfdestructed.add(call.sender)
     for address in tx_created:
         if address in created:
+            import pdb; pdb.set_trace()
             raise Exception("address created twice without being deleted: {0}".format(address))
             #print("address created twice without being deleted: {0}".format(address))
             pass
@@ -108,11 +113,12 @@ def process_transaction():
 
     for address in tx_selfdestructed:
         if address in selfdestructed:
+            import pdb; pdb.set_trace()
             raise Exception("address selfdestructed twice without being resurected in-between: {0}".format(address))
-            pass
 
         if address in created:
             created.remove(address)
+
         selfdestructed.add(address)
 
     for address in tx_ephemerals:
@@ -126,15 +132,25 @@ def process_transaction():
     tx_selfdestructed = set()
     tx_ephemerals = set() 
 
-def consume_line(line):
+def process_txs_before_trace(trace_line, start_block, break_block):
+    pass
+
+def consume_line(line, start_block, break_block):
     global last_call, created, ephemerals, reincarnations, selfdestructed, tx_calls
 
     call = MessageCall.FromCSVLine(line)
+
+    if call.block_number < start_block:
+        return False
+    if call.block_number >= break_block:
+        return True
 
     if len(tx_calls) > 0 and tx_calls[0].tx_hash != call.tx_hash:
         process_transaction()
     else:
         tx_calls.append(call)
+
+    return False
 
 progress_str = "_.."
 def advance_progress():
@@ -154,25 +170,43 @@ def main():
 
     output_file = open('output.csv', 'w')
     counter = 0
+    # start_block=9000000
+    start_block= 0 # 9000000
+    break_on_block_number = 10000000
+    should_break = False
 
-    input_files = sorted(glob.glob("data/*.csv"))
+    # txs_lines = open('tx-data.csv', 'r')
+    #input_files = sorted(glob.glob("data-traces/*.csv"))
+    # input_files = ["data-traces/data-000000000099.csv"]
+    input_files = ["mystery2.csv"]
+
     for input_file in input_files:
         source_data_file = open(input_file, 'r')
         for line in source_data_file:
             break #read first line (header)
 
         for line in source_data_file:
-            consume_line(line)
+            #process_txs_before_trace()
+
+            if consume_line(line, start_block, break_on_block_number):
+                should_break = True
+                break
+
             counter += 1
-            if counter % 10000 == 0:
+            if counter % 100000 == 0:
                 print(advance_progress(), end="\r")
 
+        if should_break:
+            break
+
+        print(input_file)
         print("ephemerals length = {}".format(len(ephemerals)))
         print("selfdestructed length ={}".format(len(selfdestructed)))
-        import pdb; pdb.set_trace()
-        foo = 'bar'
+    import pdb; pdb.set_trace()
+    foo = 'bar'
 
-    process_transaction()
+    if not should_break:
+        process_transaction()
 
 if __name__ == "__main__":
     main()
