572469eaa90a42c4f7f42fea84e99b8ba4235cc0
Jared Wasinger <j-wasinger@hotmail.com>
2022-10-28 20:50:14 +0000

    wip

---
+++ db.py
@@ -0,0 +1,59 @@
+import sqlite3
+import os
+
+from common import PrecompileCall
+
+DB_NAME='analysis.db'
+analysis_start_block=10000000 # TODO: decide proper start for this
+
+class DB():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def create_or_new():
+        already_exists = False
+        if os.path.exists(DB_NAME):
+            already_exists = True
+
+        db = DB()
+        db.connection = sqlite3.connect(DB_NAME)
+        db.cursor = db.connection.cursor()
+
+        if not already_exists:
+            print("creating new db")
+            db.createHeadBlockTable()
+            db.createCallTable()
+            db.SetHeadBlockNumber(ANALYSIS_START_BLOCK) # no precompile calls happen until a bit after 49000
+        else:
+            print("opened existing db")
+
+        return db
+
+    def createContractTable(self):
+        self.cursor.execute('''
+                CREATE TABLE contract (
+                    FOREIGN KEY(creator) REFERENCES contract(address),
+                    address TEXT NOT NULL PRIMARY KEY,
+                    created INTEGER,
+                    isEOA BOOL,
+                    contractIncarnations INTEGER,
+                    ephemeralIncarnations INTEGER,
+                    exists BOOL,
+                    invocations BOOL,
+                    aggregateGasUsage INTEGER,
+                )''')
+        self.connection.commit()
+
+    def createHeadBlockTable(self):
+        self.cursor.execute('CREATE TABLE headblock (id TEXT NOT NULL, number TEXT NOT NULL, PRIMARY KEY (id) )')
+        self.connection.commit()
+
+    def SetHeadBlockNumber(self, number: int):
+        self.head_number = number
+        self.cursor.execute('REPLACE INTO headblock (id, number) values ("headblock", "{}")'.format(number))
+        self.connection.commit()
+
+    def HeadBlockNumber(self) -> int:
+        result = self.cursor.execute('SELECT * FROM headblock').fetchall()
+        return int(result[0][1])
